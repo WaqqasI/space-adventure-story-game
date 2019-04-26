b704c6fe60d1a788d2abc8c9dc4e5d9bb96f9470
LightningsFury <light@memeware.net>
2019-04-26 16:03:40 +0100

    first commit, working boilerplate with pixi and a ghost spinning around

---
+++ README.md
@@ -0,0 +1,17 @@
+# PIXI.JS Boilerplate with Babel and Webpack.
+
+Transpiles advanced JS code into gibberish that a browser can understand. Then bundles those into a single bundle.js file and appends it to an index.html.
+
+##Start development server:
+
+```bash
+npm start
+```
+
+##Build into ./dist
+
+```bash
+npm run build
+```
+
+More TODO
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import * as PIXI from "pixi.js";
+import preloadResources from "./preloadResources.js";
+import getTexture from "./getTexture.js";
+import ghostURL from "./images/ghost.png";
+
+const app = new PIXI.Application();
+document.body.appendChild(app.view);
+
+function running() {
+  const ghostTex = getTexture("ghost");
+  // This creates a texture from a 'bunny.png' image
+  const bunny = new PIXI.Sprite(ghostTex);
+
+  // Setup the position of the bunny
+  bunny.x = app.renderer.width / 2;
+  bunny.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building
+  app.stage.addChild(bunny);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    bunny.rotation += 0.01;
+  });
+}
+
+window.addEventListener("load", () => {
+  // List of resources to load
+  const resources = [
+    {
+      name: "ghost",
+      url: ghostURL
+    }
+  ];
+
+  // Then load the images
+  preloadResources(resources, () => {
+    running();
+  });
+});
