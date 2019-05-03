6d6cba949735401f895c019e00c31d251b1e6ac7
Your Name <you@example.com>
2019-05-03 15:43:45 +0000

    🐲🚿 Checkpoint ./src/index.js:1396124/150

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import * as PIXI from "pixi.js";
 import preloadResources from "./preloadResources.js";
 import getTexture from "./getTexture.js";
-import ghostURL from "./images/spaceship.png";
+import spaceshipURL from "./images/spaceship.png";
 
 const app = new PIXI.Application({width: 1000, height: 1000});
 document.body.appendChild(app.view);
 
 function running() {
-  const ghostTex = getTexture("ghost");
+  const spaceshipTex = getTexture("ghost");
   // This creates a texture from a 'bunny.png' image
-  const bunny = new PIXI.Sprite(ghostTex);
+  const spaceship = new PIXI.Sprite(spaceshipTex);
 
   // Setup the position of the bunny
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
+  spaceship.x = app.renderer.width / 2;
+  spaceship.y = app.renderer.height / 2;
 
   // Rotate around the center
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
+  spaceship.anchor.x = 0.5;
+  spaceship.anchor.y = 0.5;
 
   // Add the bunny to the scene we are building
-  app.stage.addChild(bunny);
+  app.stage.addChild(spaceship);
 
   // Listen for frame updates
   app.ticker.add(() => {
     // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
+    spaceship.rotation += 0.01;
   });
 }
 
