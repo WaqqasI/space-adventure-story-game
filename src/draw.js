2f63d3dd6b38387300661ca0b0757dd06691fefa
Your Name <you@example.com>
2019-05-03 11:46:44 +0000

    💐🍭 Checkpoint ./src/index.js:1396124/919 ./src/draw.js:1396124/855 ./package.json:1396124/2

---
+++ src/draw.js
@@ -0,0 +1,27 @@
+import getTexture from './getTexture.js'
+import app from './index.js'
+import * as PIXI from "pixi.js";
+const draw = () => {
+  const spaceshipTex = getTexture("spaceship");
+  // This creates a texture from a 'bunny.png' image
+  const spaceship = new PIXI.Sprite(spaceshipTex);
+
+  // Setup the position of the bunny
+  spaceship.x = app.renderer.width / 2;
+  spaceship.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  spaceship.anchor.x = 0.5;
+  spaceship.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building
+  spaceship.stage.addChild(spaceship);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    spaceship.rotation += 0.01;
+  });
+}
+
+export default draw
--- src/index.js
+++ src/index.js
@@ -1,45 +1,26 @@
 import * as PIXI from "pixi.js";
 import preloadResources from "./preloadResources.js";
 import getTexture from "./getTexture.js";
-import ghostURL from "./images/spaceship.png";
-
+import spaceshipURL from "./images/spaceship.png";
+import draw from './draw.js'
 const app = new PIXI.Application({width: 1000, height: 1000});
 document.body.appendChild(app.view);
 
-function running() {
-  const ghostTex = getTexture("ghost");
-  // This creates a texture from a 'bunny.png' image
-  const bunny = new PIXI.Sprite(ghostTex);
 
-  // Setup the position of the bunny
-  bunny.x = app.renderer.width / 2;
-  bunny.y = app.renderer.height / 2;
-
-  // Rotate around the center
-  bunny.anchor.x = 0.5;
-  bunny.anchor.y = 0.5;
-
-  // Add the bunny to the scene we are building
-  app.stage.addChild(bunny);
-
-  // Listen for frame updates
-  app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-  });
-}
 
 window.addEventListener("load", () => {
   // List of resources to load
   const resources = [
     {
-      name: "ghost",
-      url: ghostURL
+      name: "spaceship",
+      url: spaceshipURL
     }
   ];
 
   // Then load the images
   preloadResources(resources, () => {
-    running();
+    draw();
   });
 });
+
+export default app
