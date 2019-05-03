cb21eacf7081e501078bc57baa5126e6bfab7c25
Your Name <you@example.com>
2019-05-03 15:40:23 +0000

    Rewound to commit 87e84d7c3eb5ef4ecd13544ae2e855f18fe1b2b3 + 0 edits

--- src/index.js
+++ src/index.js
@@ -1,50 +1,45 @@
 import * as PIXI from "pixi.js";
 import preloadResources from "./preloadResources.js";
 import getTexture from "./getTexture.js";
-import spaceshipURL from "./images/spaceship.png";
-//import draw from './draw.js'
-
+import ghostURL from "./images/spaceship.png";
 
 const app = new PIXI.Application({width: 1000, height: 1000});
-
 document.body.appendChild(app.view);
 
-const draw = () => {
-  const spaceshipTex = getTexture("spaceship");
-  console.log(spaceshipTex)
+function running() {
+  const ghostTex = getTexture("ghost");
   // This creates a texture from a 'bunny.png' image
-  const spaceship = new PIXI.Sprite(spaceshipTex);
-  console.log(spaceship)
+  const bunny = new PIXI.Sprite(ghostTex);
+
   // Setup the position of the bunny
-  spaceship.x = app.renderer.width / 2;
-  spaceship.y = app.renderer.height / 2;
-  console.log(spaceshipURL)
+  bunny.x = app.renderer.width / 2;
+  bunny.y = app.renderer.height / 2;
+
   // Rotate around the center
-  spaceship.anchor.x = 0.5;
-  spaceship.anchor.y = 0.5;
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
 
   // Add the bunny to the scene we are building
-  spaceship.stage.addChild(spaceship);
+  app.stage.addChild(bunny);
 
   // Listen for frame updates
   app.ticker.add(() => {
     // each frame we spin the bunny around a bit
-    spaceship.rotation += 0.01;
+    bunny.rotation += 0.01;
   });
 }
-
 
 window.addEventListener("load", () => {
   // List of resources to load
   const resources = [
     {
-      name: "spaceship",
-      url: spaceshipURL
+      name: "ghost",
+      url: ghostURL
     }
   ];
 
   // Then load the images
   preloadResources(resources, () => {
-    draw();
+    running();
   });
 });
